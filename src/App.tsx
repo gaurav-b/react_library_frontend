3cd62a7fccc95387f9a7c5f06f54115e61af3952
gaurav-b <sunymca619@gmail.com>
2023-04-27 18:21:08 +0530

    app with home page and several components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './layouts/navbar-and-footer/Navbar';
+import { Footer } from './layouts/navbar-and-footer/Footer';
+import { HomePage } from './layouts/home-page/HomePage';
 
-function App() {
+export const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
     </div>
   );
 }
-
-export default App;
