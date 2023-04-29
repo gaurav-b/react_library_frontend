597074bbe21cc18f987bca18ba6d41338af03648
gaurav-b <sunymca619@gmail.com>
2023-04-29 23:36:49 +0530

    installed and configured react router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,32 @@ import { Navbar } from './layouts/navbar-and-footer/Navbar';
 import { Footer } from './layouts/navbar-and-footer/Footer';
 import { HomePage } from './layouts/home-page/HomePage';
 import { SearchBooksPage } from './layouts/search-books-page/SearchBooksPage';
+import { Redirect, Route } from 'react-router-dom';
 
-export const App =() => {
+export const App = () => {
   return (
-    <div>
-      <Navbar/>
-      {/* <HomePage/> */}
-      <SearchBooksPage/>
-      <Footer/>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+
+      <div className='flex-grow-1'>
+        <switch>
+
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+
+        </switch>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- src/layouts/home-page/HomePage.tsx
+++ src/layouts/home-page/HomePage.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { Carousel } from "./components/Carousel";
 import { ExploreTopBooks } from "./components/ExploreTopBooks";
 import { Heros } from "./components/Heros";
 import { LibraryServices } from "./components/LibraryServices";
 
 export const HomePage = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    
     return (
         <>
             <ExploreTopBooks/>
